3d40f7dff3bcaba8a6308f23f6f42e152775f5f9
xiabingwu <xiabingwu@tencent.com>
2018-06-18 18:40:01 +0800

    delete

--- hoc/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App a={1} b={2} />, document.getElementById('root'));
-registerServiceWorker();
