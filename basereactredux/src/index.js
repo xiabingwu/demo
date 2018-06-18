385c880d6b0eade0c07fd864ce5fe0c15b009f2d
xiabingwu <xiabingwu@tencent.com>
2018-06-18 18:41:26 +0800

    delete

--- basereactredux/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider,connect } from 'react-redux';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
