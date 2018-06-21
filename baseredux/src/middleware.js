a302815549400950f2716a6093c95081bcc4596a
xiabingwu <xiabingwu@tencent.com>
2018-06-21 15:54:50 +0800

    demo

---
+++ baseredux/src/middleware.js
@@ -0,0 +1,36 @@
+import { compose } from './redux/index';
+let store={
+    dispatch(action){
+        return {name:action.type,data:''}
+    }
+};
+function A(dispatch){
+    return function(action){
+        console.log('=====执行A开始=====')
+        let result = dispatch(action);
+        result.data+='A';
+        return result;
+    }
+}
+function B(dispatch){
+    return function(action){
+        console.log('=====执行B开始=====')
+        // if(action.type=='endB'){
+        //     return {name:'endB',data:''}
+        // }
+        let result = dispatch(action);
+        result.data+='B';
+        return result;
+    }
+}
+function C(dispatch){
+    return function(action){
+        console.log('=====执行C开始=====')
+        let result = dispatch(action);
+        result.data+='C';
+        return result;
+    }
+}
+let newDispatch=compose(A,B,C)
+let result=newDispatch(store.dispatch)({type:'hello'})
+console.log(result)
