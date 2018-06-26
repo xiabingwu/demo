dd550820ad0ffd53de238cff383494f8418cbde4
--list <liutiantian@liutiantiandeMacBook-Pro.local>
2018-06-26 22:30:24 +0800

    demo

---
+++ basereact/src/props-immutable-1.1.js
@@ -0,0 +1,9 @@
+import React, { createElement,PureComponent } from 'react';
+import ReactDOM from 'react-dom';
+class App extends PureComponent{
+    render(){
+        //this.props.name='tencent';
+        return <p>{this.props.message.word},{this.props.name}</p>
+    }
+}
+ReactDOM.render(<App message={{word:'你好'}} name="腾讯"  />, document.getElementById('root'));
--- basereact/src/props-immutable-1.2.js
+++ basereact/src/props-immutable-1.2.js
@@ -7,7 +7,7 @@ class App extends PureComponent{
         let props={...this.props}
         console.log(Object.getOwnPropertyDescriptor(props, 'name'))
         props.name='tencent';
-        return <h1>{props.message.word} {props.name}</h1>
+        return <p>{props.message.word},{props.name}</p>
     }
 }
 ReactDOM.render(<App name="腾讯" message={{word:'你好'}} />, document.getElementById('root'));
@@ -27,11 +27,13 @@ ReactDOM.render(<App name="腾讯" message={{word:'你好'}} />, document.getEle
 
 
 
-//immutable
-//https://facebook.github.io/immutable-js/
+
 // const
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/const
 // Object.defineProperty
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
 // Object.freeze
+// Object.freeze() 方法可以冻结一个对象，冻结指的是不能向这个对象添加新的属性，不能修改其已有属性的值，不能删除已有属性，以及不能修改该对象已有属性的可枚举性、可配置性、可写性。也就是说，这个对象永远是不可变的。该方法返回被冻结的对象。
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+// immutable
+// https://facebook.github.io/immutable-js/
