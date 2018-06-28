c7d34499fb5885209c9c4786e55c19d20fe65f0b
xiabingwu <xiabingwu@tencent.com>
2018-06-28 14:21:33 +0800

    demo

--- basereduxsaga/src/fork-cancel.js
+++ basereduxsaga/src/fork-cancel.js
@@ -14,11 +14,13 @@ function* doAsync() {//异步 要明确fork的起点
         // yield cancel(task1)//不会影响doAsync和task2
         // yield fork(doResoveDelay,500)
         yield cancel()//task1和task2会被终止
+        console.log('doAsync 结束')
 }
 function* main(){
     yield call(doAsync)
-    //yield cancel()//这里取消的是main
     console.log('完成')
+    //yield cancel()//这里取消的是main
+    
 }
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
